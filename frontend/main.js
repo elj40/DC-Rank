bd4f7dfad510aac3a29c7524f8d6b160800ce70b
Benjamin Bailey <28387694@sun.ac.za>
2025-03-28 18:51:57 +0200

    image 1 displays

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,32 @@
 console.log("Hello World");
-pg = document.getElementById("pg");
-pg.innerText = "I dont care";
+
+let pg = document.getElementById("pg");
+pg.innerText = "I do care";
+
+let line = "Wonder Woman: https://comicvine.gamespot.com/a/uploads/scale_small/12/124259/8988502-ezgif-5-6e9998db59.jpg";
+let colonIndex = line.indexOf(":");
+
+if (colonIndex !== -1) {
+    let characterName = line.substring(0, colonIndex).trim();
+    let imageLink = line.substring(colonIndex + 1).trim();
+    
+    showImage(characterName, imageLink);
+}
+
+function showImage(characterName, imageLink) {
+    let img = document.createElement("img");
+    img.src = imageLink;
+    img.alt = characterName;
+    img.style.width = (window.screen.width / 2) + "px";
+    img.style.height = (window.screen.height - 300) + "px";
+
+    let container = document.getElementById("imageContainer");
+    if (!container) {
+        container = document.createElement("div");
+        container.id = "imageContainer";
+        document.body.appendChild(container);
+    }
+
+    container.innerHTML = "";
+    container.appendChild(img);
+}
